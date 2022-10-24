04df8bc4914a800c6c9e9f28934686a480d168eb
trianggautama <trianggategarutama@gmail.com>
2022-10-24 23:14:02 +0800

    add video trailer

---
+++ src/Components/VideoTrailer/VideoTrailer.js
@@ -0,0 +1,20 @@
+import { Container } from "react-bootstrap";
+import "./VideoTrailer.scss";
+
+export default function VideoTrailer(props) {
+  const youtubeURL = `https://www.youtube.com/embed/${props.youtubeId.key}`
+  return (
+    <div className="featured">
+        <div className="videoContainer">
+        <iframe
+            width="100%"
+            height="100%"
+            src={youtubeURL}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write encrypted-media; gyroscope; picture-inpicture"
+            allowFullScreen
+        />
+        </div>
+    </div>
+  );
+}
--- src/Page/MovieSelected/MovieSelected.js
+++ src/Page/MovieSelected/MovieSelected.js
@@ -9,27 +9,42 @@ import NavbarMenu from '../../Components/NavbarMenu/NavbarMenu'
 import './MovieSelected.scss'
 import axios from "axios";
 import { Container } from "react-bootstrap";
+import VideoTrailer from "../../Components/VideoTrailer/VideoTrailer";
 
 const MovieSelected = () => {
   let params = useParams();
 
   const [movie, setMovie] = useState(null);
+  const [youtubeId, setYoutubeId] = useState(null);
   const [popularMovies, setPopularMovies] = useState();
+  const [play, setPlay] = useState(false)
 
+  
   const getDetailMovie = async () => {
     const data = await axios.get(`https://api.themoviedb.org/3/movie/${params.movieId}?api_key=${process.env.REACT_APP_TMDB_APIKEY}&language=en-US`);
     setMovie(data.data);
   }
 
+  const getVideo = async () => {
+    const data = await axios.get(`https://api.themoviedb.org/3/movie/${params.movieId}/videos?api_key=${process.env.REACT_APP_TMDB_APIKEY}&language=en-US`);
+    let latestIndex = data.data.results.length -1;
+    setYoutubeId(data.data.results[latestIndex]);
+  }
 
   const getPopularMovies = async () => {
     const data = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_APIKEY}&language=en-US&sort_by=popularity.desc&page=1`);
     setPopularMovies(data.data.results.slice(6,20));
   }
 
+  const switchPlay = () => {
+    play ? setPlay(false) : setPlay(true)
+  }
+
   useEffect(() => {
     getDetailMovie();
     getPopularMovies();
+    getVideo()
+
     // console.log('movieId:'. params.movieId)
 
 
@@ -40,7 +55,12 @@ const MovieSelected = () => {
           movie && (
             <div>
               <NavbarMenu />
-              <MovieSelectedheader data={movie}/>
+              {
+                play? 
+                  <VideoTrailer  youtubeId={youtubeId}/>
+                :
+                  <MovieSelectedheader data={movie} playMovie={switchPlay}/>
+              }
               <div className="description mx-5 my-4">
                 <p className="descriptionTitle">Description</p>
                 <p>{movie.overview}</p>
